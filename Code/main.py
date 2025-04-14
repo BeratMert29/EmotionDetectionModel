c43e11ea9364a388f6c43402288201b2e857c22f
Berat Mert Gökkaya <beratmertgokkaya21@gmail.com>
2025-04-14 18:06:05 +0300

    New Commit

---
+++ Code/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:768ab23708e4adbb8c9672903cd107417a15541d132c034c61b507e5c86b3167
+size 5855
---
+++ Code/preprocess.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:89acbbabee581acc2aa828b4c24ef0d426eb37fbaa7fd4ae87df014dc41ba8f2
+size 4318
